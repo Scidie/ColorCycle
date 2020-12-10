187ba81eb253beac97a9f757379df9d5d70b2ae2
Patryk B <alexsmither84@gmail.com>
2020-12-10 16:55:11 +0000

    rebuild of program, delete most of functions

--- script.js
+++ script.js
@@ -3,52 +3,7 @@ let incrementValues = document.querySelectorAll(".input-increment-value");
 let startStopButton = document.querySelector(".start-stop-button")
 let resetButton = document.querySelector(".reset-button")
 let preview = document.querySelector(".preview-element") 
-
 let isActive = false;
-let length = colors.length;
-
-function createArrayFromInputs() {
-    let array = []
-    for (let i = 0; i < length; i++) {
-        array[i] = colors[i].value;
-    }
-    return array;
-}
-
-function incrementArray(array) {
-    let incrementedArray = [];
-    for (let i = 0; i < length; i++) {
-        if ((Number(array[i]) + Number(incrementValues[i].value)) <= 255) {
-            incrementedArray.push(Number(array[i]) + Number(incrementValues[i].value))
-        } else if ((Number(array[i]) + Number(incrementValues[i].value)) >= 255) {
-            incrementedArray.push(255)
-        } 
-    }
-    return incrementedArray;
-}
-
-function formatToCss(array) {
-    return `rgb(${array[0]}, ${array[1]}, ${array[2]})`;
-}
-
-function updateInputs(array) {
-    for (let i = 0; i < length; i++) {
-        colors[i].value = array[i];
-    }
-}
-
-function resetAll() {
-    for (let i = 0; i < length; i++) {
-        colors[i].value = 0;
-        incrementValues[i].value = 1;
-    }
-}
-
-function limitReached(array) {
-    if(array[0] === 255 && array[1] === 255 && array[2] === 255) {
-        return true;
-    } else return false;
-}
 
 function toggleButton() {
     if(isActive === true) {
@@ -58,27 +13,29 @@ function toggleButton() {
     }
 }
 
+function incrementColors() {
+    for(let i = 0; i < colors.length; i++) {
+        Number(colors[i].value) + Number(incrementValues[i].value) <= 255 ? colors[i].value = Number(colors[i].value) + Number(incrementValues[i].value) : colors[i].value = 255;  
+    }
+}
+
 startStopButton.addEventListener("click", () => {
     isActive = !isActive;
     toggleButton();
-    let arrayOfColors = createArrayFromInputs();
-    preview.style.backgroundColor = formatToCss(arrayOfColors);
+    preview.style.backgroundColor = `rgb(${colors[0].value}, ${colors[1].value}, ${colors[2].value})`;
+
 
     let interval = setInterval(function() {
-        if (isActive === false || limitReached(arrayOfColors) === true) {
+        if (isActive === false || (colors[0].value === 255 && colors[1].value === 255 && colors[2].value === 255)) {
             clearInterval(interval)
         } 
-
-        arrayOfColors = incrementArray(arrayOfColors);
-        updateInputs(arrayOfColors);
-        preview.style.backgroundColor = formatToCss(arrayOfColors);
+        
+        incrementColors();
+        preview.style.backgroundColor = `rgb(${colors[0].value}, ${colors[1].value}, ${colors[2].value})`;
     }, 25)
 })
 
 resetButton.addEventListener("click", () => {
-    resetAll()
-    let arrayOfColors = createArrayFromInputs();
-    preview.style.backgroundColor = formatToCss(arrayOfColors);
+    colors.forEach(element => element.value = 0)
+    preview.style.backgroundColor = `rgb(${colors[0].value}, ${colors[1].value}, ${colors[2].value})`;
 })
-
-
